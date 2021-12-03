e3c95a1707de1cad7dccba02d997143a8c180a7d
onedrako <onedrako@gmail.com>
2021-12-03 17:48:18 -0600

    Nav Li Working without styles

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Pokemones({pokemones}) {
   console.log(pokemones)
   return (
   <div>
-    <p>Pokemones</p>
+    <p data-testid="titulo" >Mi app de Pokemones</p>
     <ul>
       {pokemones.map(pokemon => (
         <Pokemon pokemon={pokemon} key={pokemon.name} /> )
